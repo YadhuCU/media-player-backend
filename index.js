6c03d490917a9d266bd7a7d07fd0347b3f58776c
YadhuCU <yadhuakku003@gmail.com>
2023-12-14 17:28:14 +0530

    created simple json backend server

---
+++ index.js
@@ -0,0 +1,16 @@
+const JSONServer = require("json-server");
+
+const MPServer = JSONServer.create();
+
+const router = JSONServer.router("db.json");
+
+const middleware = JSONServer.defaults();
+
+const PORT = 3000 || process.env.PORT;
+
+MPServer.use(middleware);
+MPServer.use(router);
+
+MPServer.listen(PORT, () => {
+  console.log(`Server is running at :${PORT}!!!..`);
+});
